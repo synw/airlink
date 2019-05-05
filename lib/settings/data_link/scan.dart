58d75bc6bca3b26b9b7d6d1b1dfae4684f62ad48
Win <synwe@yahoo.com>
2019-05-05 11:15:13 +0000

    Fix scan beep

--- lib/settings/data_link/scan.dart
+++ lib/settings/data_link/scan.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:qrcode_reader/qrcode_reader.dart';
 import 'package:audioplayer/audioplayer.dart';
 import '../../models/data_link.dart';
 import '../../conf.dart';
 
-Future<DataLink> scanDataLinkConfig() async {
-  AudioPlayer audioPlayer = AudioPlayer();
-  DataLink dl;
+AudioPlayer audioPlayer = AudioPlayer();
 
+Future<DataLink> scanDataLinkConfig() async {
+  DataLink dl;
   await QRCodeReader().scan().then((dataString) {
-    audioPlayer
-        .play(documentsDirectory.path + "/beep.mp3")
-        .then((_) => audioPlayer.stop());
+    //print("SCANNED $dataString");
+    playBeep();
     dynamic data = json.decode(dataString);
     dl = DataLink(
         name: data["name"].toString(),
@@ -20,6 +21,20 @@ Future<DataLink> scanDataLinkConfig() async {
         protocol: data["protocol"].toString(),
         apiKey: data["apiKey"].toString());
   });
-
   return dl;
+}
+
+Future playBeep() async {
+  final dir = documentsDirectory;
+  final file = new File("${dir.path}/beep.mp3");
+  if (!(await file.exists())) {
+    final soundData = await rootBundle.load("assets/beep.mp3");
+    final bytes = soundData.buffer.asUint8List();
+    await file.writeAsBytes(bytes, flush: true);
+  } else {
+    throw ("Mp3 file not found");
+  }
+  await audioPlayer
+      .play(file.path, isLocal: true)
+      .then((_) => audioPlayer.stop());
 }
